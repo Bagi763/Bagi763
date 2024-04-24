77e677b9d7bfb55ab224fec0d3e66bbda594d881
Bagi763 <166919061+Bagi763@users.noreply.github.com>
2024-04-24 13:55:10 -0400

    Atualizar o python_bagi.py

--- python_bagi.py
+++ python_bagi.py
@@ -1,4 +1,8 @@
-def Bagi763():
-  print("just read README.md file")
+thebagi = "just a coding dude"
 
+def Bagi763():
+  if thebagi == "just a coding dude"
+    # later I will finish this
+
+print("just read README.md file")
 Bagi763

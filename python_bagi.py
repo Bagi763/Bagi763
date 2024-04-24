482bc8002de6057819e10342fbbaf444c1e5891c
Bagi763 <166919061+Bagi763@users.noreply.github.com>
2024-04-24 16:00:23 -0400

    Atualizar o python_bagi.py

--- python_bagi.py
+++ python_bagi.py
@@ -1,8 +1,11 @@
 thebagi = "just a coding dude"
 
 def Bagi763():
-  if thebagi == "just a coding dude"
-    # later I will finish this
+  if thebagi == "just a coding dude":
+    print("I like coding ig")
+  elif thebagi == "I like python and scratch":
+    print("python is kinda meh")
+  else:
+    print("idk bagi is strange")
 
-print("just read README.md file")
-Bagi763
+print("just read README.md file" + Bagi763)
